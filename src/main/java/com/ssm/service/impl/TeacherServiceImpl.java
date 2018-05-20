622eef3ae0e796b69a03d7cb82a1800a22e0fc6f
LiuLiFei <1061334689@qq.com>
2018-05-21 02:25:43 +0800

    增加日志打印

--- src/main/java/com/ssm/service/impl/TeacherServiceImpl.java
+++ src/main/java/com/ssm/service/impl/TeacherServiceImpl.java
@@ -3,6 +3,8 @@ package com.ssm.service.impl;
 import com.ssm.entity.TeacherEntity;
 import com.ssm.mapper.TeacherMapper;
 import com.ssm.service.TeacherService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,10 +15,13 @@ public class TeacherServiceImpl  implements TeacherService {
     @Autowired
     private TeacherMapper teacherMapper;
 
+    static final Logger LOG = LoggerFactory.getLogger(TeacherServiceImpl.class);
+
     @Transactional
     @Override
     public TeacherEntity add(TeacherEntity teacherEntity) {
 
+        LOG.info("测试日志记录功能！");
         for(int i=0;i<2;i++) {
             if(i == 1) {
                 //throw  new RuntimeException("测试事物回滚");
